03b7675a7d454173f08cd4b4359f6737472e81dc
YogeshKumar995 <1413002yogesh10206@gmail.com>
2025-02-03 17:53:35 +0530

    ...

--- src/components/Forms/VideoForm.jsx
+++ src/components/Forms/VideoForm.jsx
@@ -61,7 +61,7 @@ export function VideoForm({ videoInfo }) {
                 const response = await publishVideo(formData);
                 if (response.data) {
                     toast(`Sucessfully video is uploaded`)
-                    navigate(`/channel/${videoInfo.owner.username}/videos`);
+                    navigate(`/channel`);
                 } else {
                     const errMsg = errorHandler(response);
                     setError(errMsg);

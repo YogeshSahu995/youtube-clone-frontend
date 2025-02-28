eeeb024e0b32e29dbe28fa6bf2859d935cd0ee47
YogeshKumar995 <1413002yogesh10206@gmail.com>
2025-02-28 17:02:55 +0530

    like wait functionality add

--- src/components/Video/LikeToggle.jsx
+++ src/components/Video/LikeToggle.jsx
@@ -1,24 +1,37 @@
+import { useState } from "react"
+
 export function LikeToggle({ setLike, setLikeCount, Like, likeCount, _id, fn }) {
+    const [loading, setLoading] = useState(false)
 
     const toggleLike = async () => {
-        const response = await fn(_id)
-        if(!response) return 
-        if (response?.data?.data) {
-            setLike(true)
-            setLikeCount(prev => prev + 1)
+        setLoading(true)
+        try {
+            const response = await fn(_id)
+            if(!response) return 
+            if (response?.data?.data) {
+                setLike(true)
+                setLikeCount(prev => prev + 1)
+            }
+            else {
+                setLike(false)
+                setLikeCount(prev => prev -= 1)
+            }  
+        } catch (error) {
+            console.error(error)
         }
-        else {
-            setLike(false)
-            setLikeCount(prev => prev -= 1)
+        finally{
+            setLoading(false)
         }
     }
+
     return (
         <div className="flex items-center gap-1">
             <button
                 className="text-2xl"
                 onClick={() => toggleLike()}
+                disabled = {loading}
             >
-                {Like ? (<i className="ri-heart-3-fill text-cyan-500 textEffect"></i>) : (<i className="ri-heart-3-line textEffect"></i>)}
+                {Like ? (<i className={`ri-heart-3-fill text-cyan-500 textEffect  ${loading?"cursor-wait": "cursor-pointer"}`}></i>) : (<i className={`ri-heart-3-line textEffect  ${loading?"cursor-wait": "cursor-pointer"}`}></i>)}
             </button>
             <div>
                 {likeCount}
